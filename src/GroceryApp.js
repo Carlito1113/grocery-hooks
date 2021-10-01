9960de3972f5726235a1bcf8efda527bc6cc1587
Carlito Algarin <carlito1113@yahoo.com>
2021-09-30 23:27:40 -0400

    created a custom hook for localstorage

--- src/GroceryApp.js
+++ src/GroceryApp.js
@@ -1,20 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import useItemState from './hooks/useItemState'
 import { Typography, Paper, AppBar, Toolbar, Grid } from '@material-ui/core'
 import GroceryList from './GroceryList'
 import GroceryForm from './GroceryForm'
-import { v4 as uuidv4 } from 'uuid'
 
 const GroceryApp = () => {
-  const initialList = JSON.parse(window.localStorage.getItem('list') || '[]')
+  const initialList = [{ id: 1, item: 'Kefir', completed: false }]
 
   const { list, addList, removeItem, toggleItem, editItem } = useItemState(
     initialList
   )
-
-  useEffect(() => {
-    window.localStorage.setItem('list', JSON.stringify(list))
-  }, [list])
 
   return (
     <Paper
